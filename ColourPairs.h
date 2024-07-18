9a156c65d88fb951702e0afff2bb08782e4ae768
EnciJeffrey13 <ashleyjeffrey2011@gmail.com>
2024-07-18 10:30:54 +0530

    Update ColourPairs.h

--- ColourPairs.h
+++ ColourPairs.h
@@ -1,18 +1,16 @@
-#ifndef COLORPAIR_H
-#define COLORPAIR_H
-
 #include <stdio.h>
+#include <string.h>
+#include <assert.h>
 
 #define MAX_COLORPAIR_NAME_CHARS 16
 
-// Enum definitions for major and minor colors
 typedef enum {
     WHITE,
     RED,
     BLACK,
     YELLOW,
     VIOLET,
-    NUMBER_OF_MAJOR_COLORS // Automatically keeps track of the number of major colors
+    NUMBER_OF_MAJOR_COLORS
 } MajorColor;
 
 typedef enum {
@@ -21,23 +19,62 @@ typedef enum {
     GREEN,
     BROWN,
     SLATE,
-    NUMBER_OF_MINOR_COLORS // Automatically keeps track of the number of minor colors
+    NUMBER_OF_MINOR_COLORS
 } MinorColor;
 
-// Array of major and minor color names
-extern const char* MajorColorNames[NUMBER_OF_MAJOR_COLORS];
-extern const char* MinorColorNames[NUMBER_OF_MINOR_COLORS];
-
-// Struct definition for color pair
 typedef struct {
     MajorColor majorColor;
     MinorColor minorColor;
 } ColorPair;
 
-// Function to get color pair name
-void getColorPairName(const ColorPair* colorPair, char* colorPairName);
+const char* MajorColorNames[NUMBER_OF_MAJOR_COLORS] = {
+    "White", "Red", "Black", "Yellow", "Violet"
+};
 
-// Function to print all color pairs
-void printColorPairs();
+const char* MinorColorNames[NUMBER_OF_MINOR_COLORS] = {
+    "Blue", "Orange", "Green", "Brown", "Slate"
+};
 
-#endif // COLORPAIR_H
+int GetNumberFromGivenColorPair(const ColorPair* colorPair) {
+    return colorPair->majorColor * NUMBER_OF_MINOR_COLORS + colorPair->minorColor + 1;
+}
+
+ColorPair GetColorPairFromGivenNumber(int pairNumber) {
+    ColorPair colorPair;
+    int zeroBasedPairNumber = pairNumber - 1;
+    colorPair.majorColor = (MajorColor)(zeroBasedPairNumber / NUMBER_OF_MINOR_COLORS);
+    colorPair.minorColor = (MinorColor)(zeroBasedPairNumber % NUMBER_OF_MINOR_COLORS);
+    return colorPair;
+}
+
+void PrintColorPairToString(const ColorPair* colorPair, char* buffer) {
+    snprintf(buffer, MAX_COLORPAIR_NAME_CHARS, "%s %s",
+             MajorColorNames[colorPair->majorColor],
+             MinorColorNames[colorPair->minorColor]);
+}
+
+void testNumberToColorPair(int pairNumber, MajorColor expectedMajor, MinorColor expectedMinor) {
+    ColorPair colorPair = GetColorPairFromGivenNumber(pairNumber);
+    char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
+    PrintColorPairToString(&colorPair, colorPairNames);
+    printf("Got pair %s\n", colorPairNames);
+    assert(colorPair.majorColor == expectedMajor);
+    assert(colorPair.minorColor == expectedMinor);
+}
+
+void testColorPairToNumber(MajorColor major, MinorColor minor, int expectedPairNumber) {
+    ColorPair colorPair;
+    colorPair.majorColor = major;
+    colorPair.minorColor = minor;
+    int pairNumber = GetNumberFromGivenColorPair(&colorPair);
+    printf("Got pair number %d\n", pairNumber);
+    assert(pairNumber == expectedPairNumber);
+}
+
+int main() {
+    testNumberToColorPair(4, WHITE, BROWN);
+    testNumberToColorPair(5, WHITE, SLATE);
+    testColorPairToNumber(BLACK, ORANGE, 12);
+    testColorPairToNumber(VIOLET, SLATE, 25);
+    return 0;
+}
